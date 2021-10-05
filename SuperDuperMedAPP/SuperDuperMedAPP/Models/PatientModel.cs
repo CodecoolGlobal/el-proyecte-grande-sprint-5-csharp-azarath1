fae4cf8e70073943f2a1ad6d0a4b3e539c946f4c
azarath1 <ar2oww@gmail.com>
2021-10-05 13:56:45 +0200

    feat: create PatientModel and it's properties

---
+++ SuperDuperMedAPP/SuperDuperMedAPP/Models/PatientModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperDuperMedAPP.Models
+{
+    public class PatientModel : SuperUserModel
+    {
+
+        public int SocialSecurityNumber { get; set; }
+        public ICollection<MedicationModel> Medications { get; set; }
+
+    }
+}
