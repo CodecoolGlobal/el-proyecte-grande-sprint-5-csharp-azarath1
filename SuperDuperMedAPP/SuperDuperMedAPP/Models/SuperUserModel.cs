fba1581c91d77c621331609c863ef594f3d8aec6
azarath1 <ar2oww@gmail.com>
2021-10-05 13:55:32 +0200

    feat: create abstract SuperUserModel and add it's properties

---
+++ SuperDuperMedAPP/SuperDuperMedAPP/Models/SuperUserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperDuperMedAPP.Models
+{
+    public abstract class SuperUserModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public string Username { get; set; }
+
+        public string HashPassword { get; set; }
+
+    }
+}
