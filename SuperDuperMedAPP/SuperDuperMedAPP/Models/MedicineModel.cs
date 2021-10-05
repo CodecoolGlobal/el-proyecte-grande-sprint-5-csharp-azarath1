82ea015be2daca2ea147eec1622414e8d5832498
azarath1 <ar2oww@gmail.com>
2021-10-05 13:57:27 +0200

    feat: create MedicineModel and add it's properties

---
+++ SuperDuperMedAPP/SuperDuperMedAPP/Models/MedicineModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperDuperMedAPP.Models
+{
+    public class MedicineModel
+    {
+        public int MedicineID { get; set; }
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public string DescriptionLink { get; set; }
+       
+    }
+}
