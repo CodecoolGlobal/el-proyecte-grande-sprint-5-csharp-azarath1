430125157e03eab199b83919f65df1f235dfc28b
Henrik Märcz <henrik.riki.marcz@gmail.com>
2021-10-06 11:29:13 +0200

    feat, refactor: rename Super-userModel ot User, add email/phone number properties

--- SuperDuperMedAPP/SuperDuperMedAPP/Models/Doctor.cs
+++ SuperDuperMedAPP/SuperDuperMedAPP/Models/Doctor.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace SuperDuperMedAPP.Models
 {
-    public class Doctor : SuperUserModel
+    public class Doctor : User
     {
         //public int RegistrationNumber { get; set; }
         public ICollection<Patient> Patients { get; set; }
