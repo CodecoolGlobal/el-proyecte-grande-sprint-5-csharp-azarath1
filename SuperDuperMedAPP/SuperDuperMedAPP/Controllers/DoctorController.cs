e0c3436717ce91398d2f69b8347da81ed1cdae4a
Henrik Märcz <henrik.riki.marcz@gmail.com>
2021-10-07 14:08:39 +0200

    feat: imp RegisterDoctor prototype

---
+++ SuperDuperMedAPP/SuperDuperMedAPP/Controllers/DoctorController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SuperDuperMedAPP.Models;
+using SuperDuperMedAPP.Data;
+
+namespace SuperDuperMedAPP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoctorController : ControllerBase
+    {
+        [HttpPost]
+        [Route("[action]")]
+        public ActionResult RegisterDoctor([FromBody] Doctor doctor)
+        {
+            var doctorRepository = new DoctorRepository();
+            doctorRepository.AddDoctor(doctor);
+
+            return Ok();
+        }
+    }
+}
