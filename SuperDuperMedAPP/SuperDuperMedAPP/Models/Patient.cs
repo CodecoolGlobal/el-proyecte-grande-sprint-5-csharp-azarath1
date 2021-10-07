e152b52afbea52aae6fc30e08e2553eddd52b4b8
n0rbertz <norbz123@gmail.com>
2021-10-07 11:13:41 +0200

    fix: DoctorID is nullable

--- SuperDuperMedAPP/SuperDuperMedAPP/Models/Patient.cs
+++ SuperDuperMedAPP/SuperDuperMedAPP/Models/Patient.cs
@@ -1,16 +1,21 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SuperDuperMedAPP.Models
 {
+    
     public class Patient : User
     {
-
         public int SocialSecurityNumber { get; set; }
-        public int DoctorID { get; set; }
+        public int? DoctorID { get; set; }
         public ICollection<Medication>? Medications { get; set; }
+
+
+        
+
 
     }
 }
