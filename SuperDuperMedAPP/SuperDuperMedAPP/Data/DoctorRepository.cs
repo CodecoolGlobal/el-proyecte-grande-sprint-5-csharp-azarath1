c79d1a7829b98188f5d5bc5636c566af3e438998
Henrik Märcz <henrik.riki.marcz@gmail.com>
2021-10-07 14:14:19 +0200

    feat: DoctorRepository init, AddDoctor()

---
+++ SuperDuperMedAPP/SuperDuperMedAPP/Data/DoctorRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperDuperMedAPP.Models;
+
+namespace SuperDuperMedAPP.Data
+{
+    public class DoctorRepository
+    {
+        private AppDbContext _db;
+        public DoctorRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public DoctorRepository()
+        {
+            _db = new AppDbContext();
+        }
+        
+        public void AddDoctor(Doctor doctor)
+        {
+            
+            _db.Doctors?.Add(doctor);
+            _db.SaveChanges();
+        }
+    }
+}
