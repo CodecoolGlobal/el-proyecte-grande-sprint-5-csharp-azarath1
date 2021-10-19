971ca156336d82242c838dee49af978d3ce4f438
Henrik Märcz <henrik.riki.marcz@gmail.com>
2021-10-19 14:19:05 +0200

    refactor: repository namspace

--- SuperDuperMedAPP/SuperDuperMedAPP/Controllers/PatientsController.cs
+++ SuperDuperMedAPP/SuperDuperMedAPP/Controllers/PatientsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SuperDuperMedAPP.Data;
+using SuperDuperMedAPP.Data.Repositories;
 using SuperDuperMedAPP.Models;
 
 namespace SuperDuperMedAPP.Controllers
