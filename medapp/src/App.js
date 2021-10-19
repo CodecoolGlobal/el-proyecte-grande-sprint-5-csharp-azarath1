e26295b26d5c90aabb26e7e01760c8356b576679
azarath1 <ar2oww@gmail.com>
2021-10-19 15:38:55 +0200

    feat: add tabs for main app, register routes, import react modules

--- medapp/src/App.js
+++ medapp/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {Home} from './Home';
+import {Signup} from './Signup';
+import {Navigation} from './Navigation';
+
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { Medicine } from './Medicine';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div className="container">
+     <h3 className="m-3 d-flex justify-content-center">
+       MedApp
+     </h3>
+
+     <Navigation/>
+
+     <Switch>
+       <Route path='/' component={Home} exact/>
+       <Route path='/signup' component={Signup}/>
+       <Route path='/medicines' component={Medicine}/>
+     </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
