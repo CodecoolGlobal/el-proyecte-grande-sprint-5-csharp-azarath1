a1af19e5ec27ce120c4c07a0a1837e029a5baa36
Henrik Märcz <henrik.riki.marcz@gmail.com>
2021-10-20 15:12:38 +0200

    refactor: DbSets not nullable

--- SuperDuperMedAPP/SuperDuperMedAPP/Data/AppDbContext.cs
+++ SuperDuperMedAPP/SuperDuperMedAPP/Data/AppDbContext.cs
@@ -23,11 +23,11 @@ namespace SuperDuperMedAPP.Data
         {
         }
 
-        public DbSet<Doctor>? Doctors { get; set; }
-        public DbSet<Patient>? Patients { get; set; }
-        public DbSet<Medicine>? Medicines { get; set; }
-        public DbSet<Medication>? Medications { get; set; }
-        public DbSet<User>? Users { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Medicine> Medicines { get; set; }
+        public DbSet<Medication> Medications { get; set; }
+        public DbSet<User> Users { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
