013310c9f33df09a8efa64853094f52cfde7f46d
Henrik Märcz <henrik.riki.marcz@gmail.com>
2021-11-01 13:19:32 +0100

    feat: medicine methods

--- SuperDuperMedAPP/SuperDuperMedAPP/Data/Repositories/IMedicineRepository.cs
+++ SuperDuperMedAPP/SuperDuperMedAPP/Data/Repositories/IMedicineRepository.cs
@@ -7,8 +7,9 @@ namespace SuperDuperMedAPP.Data.Repositories
     public interface IMedicineRepository
     {
         Task AddMedication(Medicine medicine);
-        Task<List<Medicine>?>  GetAllMedicine(int MedicineId);
-        Task<Medicine?>  GetMedicineById(int MedicineId);
+        Task<List<Medicine>?>  GetAllMedicine();
+        Task<List<Medicine>?> GetAllMedicineByPatientId(int medicineId);
+        Task<Medicine?>  GetMedicineById(int medicineId);
         Task UpdateMedicine(Medicine medication);
         Task DeleteMedicine(int medicineId);
     }
