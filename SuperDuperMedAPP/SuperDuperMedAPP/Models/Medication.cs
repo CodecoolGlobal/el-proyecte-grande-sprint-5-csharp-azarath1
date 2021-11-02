9b2694787d84602a3224df7a629b5f3bd1cb2de1
n0rbertz <norbz123@gmail.com>
2021-11-02 15:21:04 +0100

    resolve conflcits

--- SuperDuperMedAPP/SuperDuperMedAPP/Models/Medication.cs
+++ SuperDuperMedAPP/SuperDuperMedAPP/Models/Medication.cs
@@ -8,13 +8,11 @@ namespace SuperDuperMedAPP.Models
     public class Medication
     {
         public int MedicationID { get; set; }
-        public string Name { get => Name; set => Name = Medicine.Name; }
-       public int Doses { get; set; }
-       public string? DoctorNotes { get; set; }
+        public string Name { get; set; }
+       public int Dose { get; set; }
+       public string? DoctorNote { get; set; }
        public DateTime Date { get; set; }
-       public int MedicineID { get; set; }
        public int PatientID { get; set; }
-       public Patient Patient { get; set; }
-       public Medicine Medicine { get; set; }    
+       public Medicine Medicine { get; set; }
     }
 }
