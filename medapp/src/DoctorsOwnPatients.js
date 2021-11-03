818d714c45a0fe582988873bf572c789abe98c60
n0rbertz <norbz123@gmail.com>
2021-11-03 14:17:45 +0100

    feat: create page for viewing a doctor's own patients

---
+++ medapp/src/DoctorsOwnPatients.js
@@ -0,0 +1,8 @@
+import { useState, useEffect, } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+
+function DoctorsOwnPatientsPage() {
+    return null;
+
+}
+export default DoctorsOwnPatientsPage;
