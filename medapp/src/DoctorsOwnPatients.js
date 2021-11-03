9c3cd2c7fb8f725b268586604d7e1f11d0f3e286
n0rbertz <norbz123@gmail.com>
2021-11-03 19:50:02 +0100

    feat: create patients page for doctor

--- medapp/src/DoctorsOwnPatients.js
+++ medapp/src/DoctorsOwnPatients.js
@@ -1,8 +1,56 @@
-import { useState, useEffect, } from 'react';
 import { Modal, Button } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Table } from 'react-bootstrap';
+
 
 function DoctorsOwnPatientsPage() {
-    return null;
+    const [patientdetails, setDetails] = useState(null);
+    //   const [show, setShow] = useState(false);
+    //   const handleShow = () => setShow(true);
+    //   const handleClose = () => setShow(false);
+    const [key, id] = document.cookie.valueOf().split('=');
 
+    useEffect(() => {
+        getData();
+
+        async function getData() {
+
+            const response = await fetch(process.env.REACT_APP_BASE_URL_DOCTOR + id + '/all-patients');
+            const data = await response.json();
+            setDetails(data);
+        }
+    }, [key, id, patientdetails]);
+    if (patientdetails) {
+        return (
+            <div>
+                <h1>All Patients</h1>
+                <div className="patientdetails">
+                    <div>
+                        <Table className="mt-4" striped bordered hover size="sm">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Social Security Number</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {patientdetails.map(patient =>
+                                    <tr key={patient.id}>
+                                        <td>{patient.name}</td>
+                                        <td>{patient.socialSecurityNumber}</td>
+                                    </tr>)}
+                            </tbody>
+                        </Table>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    else {
+        return (<div></div>)
+    }
+
+
+    
 }
 export default DoctorsOwnPatientsPage;
