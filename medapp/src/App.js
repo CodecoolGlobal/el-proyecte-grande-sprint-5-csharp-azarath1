a05eb5ca4709916749f80b7c44cefb6e3763b47d
azarath1 <ar2oww@gmail.com>
2021-11-04 16:37:07 +0100

    feat: add allpatients for root path

--- medapp/src/App.js
+++ medapp/src/App.js
@@ -1,9 +1,7 @@
 import './App.css';
 import {Home} from './Home';
-import {Signup} from './Signup';
 import {Navigation} from './Navigation';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
-// import { Medicine } from './Medicine';
 import { PatientRegistration } from './PatientRegistration';
 import { DoctorRegistration } from './DoctorRegistration';
 import { LoginModal } from './LoginModal';
@@ -11,6 +9,7 @@ import { DoctorLogin } from './DoctorLogin';
 import { PatientLogin } from './PatientLogin';
 import PatientPage from './PatientDetails';
 import DoctorsOwnPatientsPage from './DoctorsOwnPatients';
+import AllPatientsPage from './AllPatientsPage';
   
 function App() {
   return (
@@ -19,16 +18,16 @@ function App() {
      <h3 className="m-3 d-flex justify-content-center">
        MedApp
      </h3>
-     <Navigation/>
+     <Navigation />
      <Switch>
        <Route path='/' component={Home} exact/>
-       <Route path='/signup' component={Signup}/>
        <Route path='/personal' component={PatientPage}/>
        <Route path='/patientRegistration' component={PatientRegistration}/>
        <Route path='/doctorRegistration' component={DoctorRegistration} />
        <Route path='/login' component={LoginModal}/>
        <Route path='/doctorLogin' component={DoctorLogin} />
        <Route path='/patientLogin' component={PatientLogin} />
+       <Route path='/allpatients' component={AllPatientsPage} />
        <Route path='/mypatients' component={DoctorsOwnPatientsPage} />
      </Switch>
     </div>
