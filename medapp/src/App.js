eb15e2b6db908a9b8d03085fc02e19389deffc12
azarath1 <ar2oww@gmail.com>
2021-11-15 12:09:52 +0100

    refactor: home imports as function

--- medapp/src/App.js
+++ medapp/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Home} from './Home';
+import Home from './Home';
 import {Navigation} from './Navigation';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import { PatientRegistration } from './PatientRegistration';
