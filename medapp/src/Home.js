b1970ca4f6af5eca27bc5e200cd8bfe970cc5e88
azarath1 <ar2oww@gmail.com>
2021-11-15 12:09:18 +0100

    refactor: Home.js with react hooks

--- medapp/src/Home.js
+++ medapp/src/Home.js
@@ -1,64 +1,80 @@
-import React,{Component} from 'react';
+import { useState, useEffect, } from 'react';
 import {Table} from 'react-bootstrap';
 
-export class Home extends Component{
+function Home() {
 
-    constructor(props){
-        super(props);
-        this.state={coro:[], corohun:[]}
-    }
+    const [coronaDetails, setDetails] = useState(null);
+    const [coronaHunDetails, setHunDetails] = useState(null);
+    useEffect(() => {
+        let isActive = true;
+        getData();
+    
+        async function getData() {
+    
+          const response = await fetch("https://coronavirus.m.pipedream.net/",
+          { 
+            method: 'GET',
+            headers: { 'Accept': 'application/json', 
+                       'Content-Type': 'application/json' }
+            });
+          const data = await response.json();
+          const coro = data.summaryStats.global;
+          const coroHun = data.rawData.find(item => item.Country_Region === "Hungary");
+          if (isActive){
+            setDetails(coro);
+            setHunDetails(coroHun);
+          }
+          return () => {
+            isActive = false;
+          };
+        }
+      }, []);
+        if(coronaDetails && coronaHunDetails){
+            return (
+                    <div className="justify-content-center">
+                        <h2 className="text-danger">Welcome to SuperduperMedapp!</h2>
+                        <div className="card text-white bg-secondary mb-3">
+                            <div className="card-header"><h4>Current Coronavirus Statistics</h4></div>
+                                <div className="card-body">
+                                    <Table className="mt-4" striped bordered hover size="sm">
+                                        <thead>
+                                            <tr>
+                                                <th>Confirmed Globally</th>
+                                                <th>Deaths</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td className="text-danger">{coronaDetails.confirmed}</td>
+                                                <td>{coronaDetails.deaths}</td>
+                                            </tr>
+                                        </tbody>
+                                        <thead>
+                                            <tr>
+                                                <th>Confirmed in Hungary</th>
+                                                <th>Deaths in Hungary</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td className="text-danger">{coronaHunDetails.Confirmed}</td>
+                                                <td>{coronaHunDetails.Deaths}</td>
+                                            </tr>
+                                        </tbody>
+                                    </Table>
+                                </div>
+                        </div>
+                        
+                        <h4>Placeholder articles</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pulvinar urna nisi, non interdum ipsum pulvinar ullamcorper. Suspendisse eros lorem, mollis vel pellentesque molestie, dictum dictum lorem. Duis ipsum dui, accumsan at eleifend quis, sodales dignissim eros. Nunc at sollicitudin leo, vel sagittis ligula. Nullam massa velit, mattis eget dolor nec, posuere molestie ante. Maecenas in ipsum sit amet justo ultrices placerat dignissim ac nibh. Nunc a felis lacus.</p>
+                    </div>   
+            )
+        }
+        else{
+          return (
+            <div></div>   
+          )
+        }
+      }
 
-    refreshList(){
-        fetch("https://coronavirus.m.pipedream.net/",
-               { method: 'GET',
-               headers: {
-               'Accept': 'application/json',
-               'Content-Type': 'application/json'
-               }})
-           .then(response=>response.json())
-           .then(data=>{
-           this.setState({coro:data.summaryStats.global, corohun:data.rawData.find(item => item.Country_Region === "Hungary")});
-       });
-   }
-
-   componentDidMount(){
-       this.refreshList();
-   }
-
-    render(){
-        return(
-            <div className="mt-5 justify-content-center">
-                <h2>Welcome to SuperduperMedapp Site!</h2>
-                <h4>Current Coronavirus Stats</h4>
-                <Table className="mt-4" striped bordered hover size="sm">
-                    <thead>
-                        <tr>
-                            <th>Confirmed</th>
-                            <th>Deaths</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>{this.state.coro.confirmed}</td>
-                            <td>{this.state.coro.deaths}</td>
-                        </tr>
-                    </tbody>
-                    <thead>
-                        <tr>
-                            <th>Confirmed in Hungary</th>
-                            <th>Deaths in Hungary</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>{this.state.corohun.Confirmed}</td>
-                            <td>{this.state.corohun.Deaths}</td>
-                        </tr>
-                    </tbody>
-                </Table>
-                <h4>Placeholder articles</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pulvinar urna nisi, non interdum ipsum pulvinar ullamcorper. Suspendisse eros lorem, mollis vel pellentesque molestie, dictum dictum lorem. Duis ipsum dui, accumsan at eleifend quis, sodales dignissim eros. Nunc at sollicitudin leo, vel sagittis ligula. Nullam massa velit, mattis eget dolor nec, posuere molestie ante. Maecenas in ipsum sit amet justo ultrices placerat dignissim ac nibh. Nunc a felis lacus.</p>
-            </div>
-        )
-    }
-}
+export default Home;
