bb7b5c05cd94967c9ae16a562465b79062a51e5d
Henrik Märcz <henrik.riki.marcz@gmail.com>
2021-11-16 11:49:23 +0100

    feat: UserToAuthDTO

---
+++ SuperDuperMedAPP/SuperDuperMedAPP/Models/DTO/GetUserToAuthDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace SuperDuperMedAPP.Models.DTO
+{
+    public class GetUserToAuthDTO
+    {
+        public int Id { get; set; }
+        public string HashPassword { get; set; }
+        public string Role { get; set; }
+    }
+}
