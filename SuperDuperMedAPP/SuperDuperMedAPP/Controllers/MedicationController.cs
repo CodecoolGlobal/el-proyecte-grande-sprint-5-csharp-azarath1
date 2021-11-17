991406db548f8e59fa3d9238ee38bcb8c6fe8c3e
Henrik Märcz <henrik.riki.marcz@gmail.com>
2021-11-17 12:48:21 +0100

    feat: imp medication controller

---
+++ SuperDuperMedAPP/SuperDuperMedAPP/Controllers/MedicationController.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SuperDuperMedAPP.Data.Services;
+using SuperDuperMedAPP.Infrastructure;
+using SuperDuperMedAPP.Models.DTO;
+
+namespace SuperDuperMedAPP.Controllers
+{
+    [ApiController]
+    public class MedicationController : ControllerBase
+    {
+        private IMedicationService _services;
+
+        public MedicationController(IMedicationService services)
+        {
+            _services = services;
+        }
+
+
+        [HttpPost]
+        [Route("doctor/{id:int}/medication/add")]
+        public async Task<ActionResult> AddMedication([FromBody] AddMedicationDTO medicationDto)
+        {
+            var medicine = await _services.GetMedicineById(medicationDto.MedicineID);
+            if (medicine == null)
+            {
+                return NotFound();
+            }
+
+            var medication = medicationDto.ToMedication(medicine);
+            await _services.AddMedication(medication);
+            return NoContent();
+        }
+
+        //change url?
+        [HttpPut]
+        [Route("doctor/{id:int}/medication/{medicationId}/edit-dosage")]
+        public async Task<ActionResult> ModifyMedicationDosage([FromRoute] int medicationId,
+            [FromBody] string newDosage)
+        {
+            var medication = await _services.GetMedicationById(medicationId);
+            if (medication == null)
+            {
+                return NotFound();
+            }
+
+            await _services.EditMedicationDosage(medicationId, newDosage);
+            return NoContent();
+        }
+
+        //change url?
+        [HttpPut]
+        [Route("doctor/{id:int}/medication/{medicationId}/edit-note")]
+        public async Task<ActionResult> ModifyMedicationNote([FromRoute] int id, [FromRoute] int medicationId,
+            [FromBody] string newNote)
+        {
+            var medication = await _services.GetMedicationById(medicationId);
+            if (medication == null)
+            {
+                return NotFound();
+            }
+
+            await _services.EditMedicationNote(medicationId, newNote);
+            return NoContent();
+        }
+
+        //change url?
+        [HttpDelete]
+        [Route("doctor/{id:int}/medication/{medId:int}/delete")]
+        public async Task<ActionResult> DeleteMedication([FromRoute] int id, [FromRoute] int medId)
+        {
+            var medication = await _services.GetMedicationById(medId);
+            if (medication == null)
+            {
+                return NotFound();
+            }
+
+            await _services.DeleteMedication(medId);
+            return NotFound();
+        }
+
+        //change url?
+        [Route("doctor/{id:int}/patients-medication/{medicationId:int}")]
+        public async Task<ActionResult> GetPatientsMedicationSingle([FromRoute] int id, [FromRoute] int medicationId)
+        {
+            var medication = await _services.GetMedicationById(medicationId);
+
+            if (medication == null)
+            {
+                return NotFound();
+            }
+
+            var response = medication.ToGetPatientsMedicationSingleDto();
+            return Ok(response);
+        }
+
+        //change url?
+        [Route("doctor/{id:int}/patients-medications/{patientId:int}/{pageNumber:int}")]
+        public async Task<ActionResult> GetPatientsMedicationAll([FromRoute] int id, [FromRoute] int patientId,
+            [FromRoute] int pageNumber)
+        {
+            var medications = await _services.GetAllMedicationByPatientId(patientId, pageNumber);
+
+            if (medications == null)
+            {
+                return NotFound();
+            }
+
+            var response = medications.ToGetPatientsMedicationAllDTO();
+            return Ok(response);
+        }
+
+        //to medication controller
+        [Route("patient/{patientId:int}/medication/{pageNumber:int}")]
+        public async Task<ActionResult> GetPatientsMedication([FromRoute] int patientId, [FromRoute] int pageNumber)
+        {
+            var userMedication = await _services.GetAllMedicationByPatientId(patientId, pageNumber);
+            if (userMedication == null)
+            {
+                return NoContent();
+            }
+
+            return Ok(userMedication);
+        }
+    }
+}
