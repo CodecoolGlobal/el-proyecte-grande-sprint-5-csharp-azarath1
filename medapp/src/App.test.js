e1f04672775d5c5681fcd763388afbbcdfcbfcca
azarath1 <ar2oww@gmail.com>
2021-11-17 08:58:02 +0100

    test: add App dom tests

--- medapp/src/App.test.js
+++ medapp/src/App.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './App.js';
 
-test('renders learn react link', () => {
+test('renders navbar', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Login/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders corona statistics tab', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Coronavirus/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Placeholder title', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Placeholder/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Placeholder body', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Lorem/i);
   expect(linkElement).toBeInTheDocument();
 });
