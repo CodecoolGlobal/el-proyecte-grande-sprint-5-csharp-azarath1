bbe4e2346e834f390a3d94012ff55447e7277f96
azarath1 <ar2oww@gmail.com>
2021-11-18 15:23:29 +0100

    test: detailspage test modified

--- medapp/src/App.test.js
+++ medapp/src/App.test.js
@@ -3,14 +3,11 @@ import App from './App.js';
 
 test('renders index page', () => {
   render(<App />);
-  const registerElement = screen.getByText(/Sign Up/i);
-  const loginElement = screen.getByText(/Login/i);
+  
   const coronaElement = screen.getByText(/Coronavirus/i);
   const placeholderTitleElement = screen.getByText(/Placeholder/i);
   const articlePhraseElement = screen.getByText(/Lorem/i);
 
-  expect(registerElement).toBeInTheDocument();
-  expect(loginElement).toBeInTheDocument();
   expect(coronaElement).toBeInTheDocument();
   expect(placeholderTitleElement).toBeInTheDocument();
   expect(articlePhraseElement).toBeInTheDocument();
