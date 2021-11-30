02176de7d3dc05f895e76ce2a180e6d0094aec2e
azarath1 <ar2oww@gmail.com>
2021-11-30 13:01:15 +0100

    refactor: patientRegistration function render form

--- medapp/src/PatientRegistration.js
+++ medapp/src/PatientRegistration.js
@@ -19,6 +19,92 @@ function PatientRegistration() {
 
 
 
+
+    return (
+        <form action="submit">
+            <div>
+            <label>
+                Social Security Number:
+            </label>
+            <br/>
+                <input
+                    name="socialSecurityNumber"
+                    type="number"
+                    placeholder="000999000"
+                    onChange={event => setSecurityNumber(event.target.value)}
+                     />
+            </div>
+            <div>
+            <label>
+                Name:
+            </label>
+            <br/>
+                <input
+                    name="name"
+                    type="textarea"
+                    placeholder="Example Béla"
+                    onChange={event => setName(event.target.value)} />
+            </div>
+            <div>
+            <label>
+                Date of Birth:
+            </label>
+            <br/>
+                <input
+                    name="dateOfBirth"
+                    type="date"
+                    placeholder="1991.01.01"
+                    onChange={event => setBirthDate(event.target.value)} />
+            </div>
+            <div>
+            <label>
+                Email:
+            </label>
+            <br/>
+                <input
+                    name="email"
+                    type="textarea"
+                    placeholder="mail@mail.com"
+                    onChange={event => setEmail(event.target.value)} />
+            </div>
+            <div>
+            <label>
+                Phone Number:
+            </label>
+            <br/>
+                <input
+                    name="phoneNumber"
+                    type="textarea"
+                    placeholder="+36304443333"
+                    onChange={event => setPhoneNumber(event.target.value)} />
+            </div>
+            <div>
+            <label>
+                Username:
+            </label>
+            <br/>
+                <input
+                    name="username"
+                    type="textarea"
+                    placeholder="SnoopDoge"
+                    onChange={event => setUserName(event.target.value)} />
+            </div>
+            <div>
+            <label>
+                Password:
+            </label>
+            <br/>
+                <input
+                    name="password"
+                    type="password"
+                    onChange={event => setPassword(event.target.value)}
+                    />
+            </div>
+            <br />
+            <input type="submit" value="Submit" onClick={handleSubmit} />
+        </form>
+    );
+
     
 
 }
