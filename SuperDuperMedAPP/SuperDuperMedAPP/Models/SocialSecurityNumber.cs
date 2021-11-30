4db842bd79fad94a171b45c18aa9c97c851477f7
Henrik Märcz <henrik.riki.marcz@gmail.com>
2021-11-30 11:25:49 +0100

    feat: Patient Social security number validated at reg.

---
+++ SuperDuperMedAPP/SuperDuperMedAPP/Models/SocialSecurityNumber.cs
@@ -0,0 +1,8 @@
+﻿namespace SuperDuperMedAPP.Models
+{
+    public class SocialSecurityNumber
+    {
+        public long ID { get; set; }
+        public string SocialSecurityNum { get; set; }
+    }
+}
