e29b7a2cd7b60b88947e6ec110fcfc03d5b27ae5
Henrik Märcz <henrik.riki.marcz@gmail.com>
2021-12-01 14:39:22 +0100

    refactor: DateOFBirth set to nullable

--- SuperDuperMedAPP/SuperDuperMedAPP/Models/DTO/PatientDetailDTO.cs
+++ SuperDuperMedAPP/SuperDuperMedAPP/Models/DTO/PatientDetailDTO.cs
@@ -6,7 +6,7 @@ namespace SuperDuperMedAPP.Models.DTO
     public class PatientDetailDTO
     {
         public string? Name { get; set; }
-        public string DateOfBirth { get; set; }
+        public string? DateOfBirth { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
         public string? SocialSecurityNumber { get; set; }
