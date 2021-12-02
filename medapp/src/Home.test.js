155038ea5930ebf2f3b5a3f95002f8a561f0374c
Henrik Märcz <henrik.riki.marcz@gmail.com>
2021-12-02 11:22:38 +0100

    refactor: delete medApp

--- medapp/src/Home.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Home from './Home.js';
-
-test('renders cardstats', () => {
-    render(<Home />);
-    const welcomeText = screen.getByText(/SuperduperMedapp!/i);
-    expect(welcomeText).toBeInTheDocument();
-  });
